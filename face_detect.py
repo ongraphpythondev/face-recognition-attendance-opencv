fe9aaf92e8d57d583208b36c3ca59bf787562014
ongraphpythondev <pythondev@ongraph.com>
2020-10-21 16:28:52 +0530

    first commit

---
+++ face_detect.py
@@ -0,0 +1,83 @@
+import cv2
+import numpy as np
+import face_recognition
+import os
+from datetime import datetime
+
+#  Get all image from directory and get image name to display valid user name
+
+path = 'image'
+images = []
+classNames = []
+image_list = os.listdir(path)
+print(image_list)
+for cl in image_list:
+    curImg = cv2.imread(f'{path}/{cl}')
+    images.append(curImg)
+    classNames.append(os.path.splitext(cl)[0])
+    print(classNames)
+
+
+# Its encode the user image to recognise the valid user
+
+def findEncodings(images):
+    encodeList = []
+
+    for img in images:
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        encode = face_recognition.face_encodings(img)[0]
+        encodeList.append(encode)
+    return encodeList
+
+
+# Save valid user data in csv file like attendance
+def markAttendance(name):
+    with open('Attendance.csv', 'r+') as f:
+        myDataList = f.readlines()
+        nameList = []
+        for line in myDataList:
+            entry = line.split(',')
+            nameList.append(entry[0])
+        if name not in nameList:
+            now = datetime.now()
+            dtString = now.strftime('%H:%M:%S')
+            f.writelines(f'\n{name},{dtString}')
+
+
+encodeListKnown = findEncodings(images)
+print('Encoding Complete')
+
+# detact user face from webcam  and capture it and change the color after that encode the user face and match
+# distance from other images face
+
+cap = cv2.VideoCapture(0)
+
+while True:
+    success, img = cap.read()
+    # img = captureScreen()
+    imgS = cv2.resize(img, (0, 0), None, 0.25, 0.25)
+    imgS = cv2.cvtColor(imgS, cv2.COLOR_BGR2RGB)
+
+    facesCurFrame = face_recognition.face_locations(imgS)
+    encodesCurFrame = face_recognition.face_encodings(imgS, facesCurFrame)
+
+    # getting location of face and encode of user face
+
+    for encodeFace, faceLocation in zip(encodesCurFrame, facesCurFrame):
+        matches = face_recognition.compare_faces(encodeListKnown, encodeFace)
+        faceDis = face_recognition.face_distance(encodeListKnown, encodeFace)
+        matchIndex = np.argmin(faceDis)
+
+        if matches[matchIndex]:
+            name = classNames[matchIndex].upper()
+            # if face matched then square box in green color show on face with name
+            y1, x2, y2, x1 = faceLocation
+            y1, x2, y2, x1 = y1 * 4, x2 * 4, y2 * 4, x1 * 4
+            cv2.rectangle(img, (x1, y1), (x2, y2), (0, 255, 0), 2)
+            cv2.rectangle(img, (x1, y2 - 35), (x2, y2), (0, 255, 0), cv2.FILLED)
+            cv2.putText(img, name, (x1 + 6, y2 - 6), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 255, 255), 2)
+            markAttendance(name)
+
+    cv2.imshow('Webcam', img)
+    cv2.waitKey(1)
+
